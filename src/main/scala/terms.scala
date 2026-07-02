3d12e8f52157751956488f6a8ba8dd230c349348
Nimodimon <nimodimon@icloud.com>
2026-07-02 16:46:17 +0300

    refactor: remove TermError and VariableNameError

--- src/main/scala/terms.scala
+++ src/main/scala/terms.scala
@@ -3,17 +3,16 @@ object terms:
 
   sealed trait LambdaError
 
-  case class Variable private(name: String) extends Term:
+  case class Variable private (name: String) extends Term:
     override def toString: String = name
 
-  case class VariableNamingError(name: String) extends LambdaError:
-    override def toString: String = s"'$name' is incorrect naming for variable. Please, use only latin symbols!"
-
   object Variable:
-    def apply(name: String): Either[LambdaError, Term] =
-      if name.matches("^[a-zA-Z]+$") then
-        Right(new Variable(name))
-      else Left(VariableNamingError(name))
+    def apply(name: String): Either[String, Variable] =
+      if name.matches("^[a-zA-Z]+$") then Right(new Variable(name))
+      else
+        Left(
+          s"'$name' is incorrect naming for variable. Please, use only latin symbols!"
+        )
 
   case class Application(left: Term, right: Term) extends Term:
     override def toString: String = s"($left $right)"
